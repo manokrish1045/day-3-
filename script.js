d79f7e613f5debb82f9c7c4335c704bbfe0867e7
manokrish1045 <109267208+manokrish1045@users.noreply.github.com>
2022-07-21 20:40:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var arr={
+           
+  
+    "name"         : "Mano krishnan R ",
+    "Qualification": "BscPHYSICS",
+    "email"        :  "abc@gmail.com",
+    "phone"        :  "4321",
+    "location"     :  "Sithurajapuram",
+      "address"    :  "abc street",
+      "postalCode" :  "94115",
+      "city"       :  "Sivakasi",
+      "countryCode":  "IN",
+      "region"     :  "Virudhunagar",
+  
+    
+};
+           console.log(arr);
